50638496a546ed24682d2be9f15c3f0749f29f30
Konstantin <kprudnikov@gmail.com>
2016-11-11 21:11:57 +0200

    initial setup and outline

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+// const concat = require('gulp-concat');
+const watch = require('gulp-watch');
+var del = require('del');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/main.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./sass'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('sass/**/*.scss', ['autoprefix']);
+});
+
+gulp.task('autoprefix', ['sass'], () => {
+    gulp.src('sass/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer('last 25 versions'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('default', ['sass', 'autoprefix','watch']);
+
+// gulp.task('stream', function () {
+//     // Endless stream mode
+//     return watch('css/**/*.css', { ignoreInitial: false })
+//         .pipe(gulp.dest('build'));
+// });
+
+// gulp.task('callback', function () {
+//     // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+//     return watch('css/**/*.css', function () {
+//         gulp.src('css/**/*.css')
+//             .pipe(gulp.dest('build'));
+//     });
+// });
+
+// //
+// //
+
+// // 'use strict';
+
+// // var gulp = require('gulp');
+// // var sass = require('gulp-sass');
+
+// gulp.task('sass', function () {
+//   return gulp.src('./sass/**/*.scss')
+//     .pipe(sass.sync().on('error', sass.logError))
+//     .pipe(gulp.dest('./css'));
+// });
+
+// gulp.task('sass:watch', function () {
+//   gulp.watch('./sass/**/*.scss', ['sass']);
+// });
+
+// //
+// //
+
+// // const gulp = require('gulp');
+// // const sourcemaps = require('gulp-sourcemaps');
+// // const autoprefixer = require('gulp-autoprefixer');
+// // const concat = require('gulp-concat');
+
+// gulp.task('default', () =>
+//     gulp.src('src/**/*.css')
+//         .pipe(sourcemaps.init())
+//         .pipe(autoprefixer())
+//         .pipe(concat('all.css'))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest('dist'))
+// );
